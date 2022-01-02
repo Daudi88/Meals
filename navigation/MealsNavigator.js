df6173186cb216760159dc74a6892cff84b0adcd
David Ström <davidstrom@Davids-MBP.lan>
2022-01-02 11:33:11 +0100

    Add default navigation options

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,13 +1,24 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import colors from '../constants/colors';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: CategoryMealsScreen,
-  MealDetail: MealDetailScreen,
-});
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    CategoryMeals: CategoryMealsScreen,
+    MealDetail: MealDetailScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? colors.primary : '',
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : colors.primary,
+    },
+  }
+);
 
 export default createAppContainer(MealsNavigator);
